5c204e94aa427a241b8564fddaf6eecb3310e9bc
StKondrat <kndrt13@icloud.com>
2026-03-23 00:58:24 +0300

    add some tests in main.cpp

--- main.cpp
+++ main.cpp
@@ -7,12 +7,51 @@ bool testEmptyVector()
   return v.isEmpty();
 }
 
+bool testGetSize()
+{
+  topit::Vector< int > v;
+  v.pushBack(1);
+  bool res = v.getSize() == 1;
+  return res;
+}
+
+bool testGetCapacity()
+{
+  topit::Vector< int > v;
+  v.pushBack(1);
+  bool res = v.getCapacity() == 1;
+  return res;
+}
+
+bool testPushBack()
+{
+  topit::Vector< int > v;
+  v.pushBack(1);
+  v.pushBack(2);
+  bool res = v.getSize() == 2 && v.getCapacity() == 2;
+  return res;
+}
+
+bool testPopBack()
+{
+  topit::Vector< int > v;
+  v.pushBack(1);
+  v.pushBack(2);
+  v.popBack();
+  bool res = v.getSize() == 1 && v.getCapacity() == 2;
+  return res;
+}
+
 int main()
 {
   using test_t = std::pair< const char *, bool(*)() >;
   test_t tests[] = 
   {
-    { "Empty vector", testEmptyVector }
+    { "Empty vector", testEmptyVector },
+    { "Get size", testGetSize },
+    { "Get capacity", testGetCapacity },
+    { "Push back", testPushBack },
+    { "Pop back", testPopBack }
   };
 
   const size_t count = sizeof(tests) / sizeof(test_t);
